958dca7359e4051869338281e0155cf4a615456d
facebook <113114688+vinanshu@users.noreply.github.com>
2024-03-30 01:30:18 +0800

    don eye school work
    school purpose

---
+++ contact.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById('contact-form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
+        sendEmail(); // Call the sendEmail function
+    });
+});
+
+function sendEmail() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
+    var subject = "Contact Request from Don-Eye Store Website";
+
+    var mailtoLink = "mailto:villasandonnahjane@gmail.com" +
+        "?subject=" + encodeURIComponent(subject) +
+        "&body=" + encodeURIComponent("Name: " + name + "\nEmail: " + email + "\nPhone: " + phone + "\nMessage: " + message);
+
+    window.location.href = mailtoLink;
+}
